9fdc9cfb7148941b92981ab6b8f41c4c27221ce8
anishmo99 <ani10sh@gmail.com>
2020-05-20 15:59:01 +0530

    Update stockSpanner.cpp

--- stockSpanner.cpp
+++ stockSpanner.cpp
@@ -18,8 +18,43 @@ public:
     }
 };
 
-/**
- * Your StockSpanner object will be instantiated and called as such:
- * StockSpanner* obj = new StockSpanner();
- * int param_1 = obj->next(price);
+/*
+ //geeksforgeeks
+ #include <iostream>
+ #include <stack>
+ #include <vector>
+ using namespace std;
+
+ int main() {
+     int cases;
+     cin>>cases;
+     while(cases--)
+     {
+         int n;
+         cin>>n;
+         int x;
+         vector<int> output;
+         stack<int> prices;
+         stack<int> weights;
+
+         for(int i=0;i<n;i++)
+         {
+             cin>>x;
+             int w=1;
+             while(!prices.empty()&&prices.top()<=x)
+             {
+                 prices.pop();
+                 w+=weights.top();
+                 weights.pop();
+             }
+             prices.push(x);
+             weights.push(w);
+             output.push_back(w);
+         }
+         for(auto& i:output)
+             cout<<i<<" ";
+         cout<<endl;
+     }
+     return 0;
+ }
  */
