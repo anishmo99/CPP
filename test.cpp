51eb119dd277d350d2c16b4f64be93c3ea1461a2
anishmo99 <ani10sh@gmail.com>
2020-05-25 16:05:48 +0530

    removed unncessary files

--- test.cpp
+++ test.cpp
@@ -1,7 +1,36 @@
 #include <iostream>
+#include <string>
+#include <cstring>
+#include <vector>
+#include <cstdio>
+#include <algorithm>
 using namespace std;
+
+bool isPangram(string s)
+{
+	int hash[26]={0};
+	transform(s.begin(),s.end(),s.begin(),::tolower);
+	for(char& c:s)
+		hash[c-'a']++;
+	for(int& i:hash)
+		if(i>=1)
+			continue;
+		else return false;
+
+	return true;
+}
 
 int main()
 {
-    printf("hello world");
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	
+	string s;
+	getline(cin,s);
+	if(isPangram(s))
+		cout<<"pangram\n";
+	else
+		cout<<"not pangram\n";
 }
--- testNewFile.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main()
-{
-    cout<<"test new file "<<endl;
-    return 0;
-}
