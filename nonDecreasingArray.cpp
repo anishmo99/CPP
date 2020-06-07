1b73ef2334b5bca4188cc1cbcb8ad9157b5f5ab1
anishmo99 <ani10sh@gmail.com>
2020-06-07 11:09:52 +0530

    Create nonDecreasingArray.cpp

---
+++ nonDecreasingArray.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int p = -1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i-1] > nums[i]) {
+                if (p != -1) return false;
+                else p = i;
+            }
+        }
+        return (p == -1 ||   // no reversed pair
+                p == 1 || p == nums.size() - 1 ||  // reversed pair is first or last element
+                nums[p - 2] <= nums[p] || nums[p - 1] <= nums[p + 1]); // normal case range [p-2 --> p+1] all valid
+    }
+};
+
