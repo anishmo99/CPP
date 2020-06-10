5c84c03fbdd8704854eefd5762fca14b94c37c8b
anishmo99 <ani10sh@gmail.com>
2020-06-10 09:25:26 +0530

    Update checkStringRotations.cpp

--- checkStringRotations.cpp
+++ checkStringRotations.cpp
@@ -1,3 +1,28 @@
+// CPP approach
+#include <iostream>
+using namespace std;
+
+bool isRotation(string s1,string s2)
+{
+    if(s1.length()!=s2.length())
+        return false;
+    string temp=s1+s1;
+    return temp.find(s2)!=string::npos;
+}
+
+int main() {
+    int cases;
+    cin>>cases;
+    while(cases--)
+    {
+        string s1,s2;
+        cin>>s1>>s2;
+        cout<<isRotation(s1,s2)<<endl;
+    }
+    return 0;
+}
+
+// C approach
 #include <iostream>
 #include <cstring>
 
