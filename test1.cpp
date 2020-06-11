3c823c69da880faa9ea9b8cff4c4664a2a2510d1
anishmo99 <ani10sh@gmail.com>
2020-06-11 18:48:42 +0530

    Update test1.cpp

--- test1.cpp
+++ test1.cpp
@@ -1,65 +1,22 @@
-#include <iostream>
-#include <string>
-#include <cstring>
-#include <vector>
-#include <cstdio>
-#include <algorithm>
-#include <utility>
-#include <map>
-#include <set>
-#include <queue>
-using namespace std;
-
-typedef long long ll;
-
-#define EB emplace_back
-#define PB push_back
-#define F first
-#define S second
-
-
-int numberOfCoins(vector<int> coins,int amount,int n)
-{
-    int dp[amount][n];
-
-    for(int i=0;i<n;i++)
-        dp[0][i]=1;
-
-    for(int i=1;i<n;i++)
-    {
-        for(int j=0;j<=amount;j++)
+int isPowerOfTwo(int n) {
+        if(n==1)
+        return 1;
+        if(n<=0)    return false;
+        while(n%2==0)
         {
-            if(i==0)
-
+            n/=2;
         }
+        if(n==1)
+            return 1;
+        return 0;
     }
 
-}
 
-
-int main()
+int* isPower(int n,int *arr, int* result_count)
 {
-#ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-#endif
-
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int cases;
-    cin >> cases;
-    while(cases--){
-        int amount;
-        cin>>amount;
-        int n;
-        cin>>n;
-        vector<int> coins(n);
-        for(int i=0;i<n;i++)
-            cin>>v[i];
-        cout<<numberOfCoins(coins,amount,n);
-        cout<<endl;
+    for(int i=0;i<n;i++)
+    {
+        result.count[i]=isPowerOfTwo(arr[i]);
     }
-    return 0;
+    return *result_count;
 }
