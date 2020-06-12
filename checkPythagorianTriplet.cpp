a06840833ef687b5459833ef2dacbb3ef0d1753c
anishmo99 <ani10sh@gmail.com>
2020-06-12 12:37:01 +0530

    Create checkPythagorianTriplet.cpp

---
+++ checkPythagorianTriplet.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+bool isPythTrip(int arr[],int n)
+{
+    for(int i{};i<n;i++)
+    {
+        arr[i]=pow(arr[i],2);
+    }
+    sort(arr,arr+n);
+    for(int i=n-1;i>=0;i--)
+    {
+        int l=0;
+        int r=n-1;
+        while(l<r)
+        {
+            if(arr[l]+arr[r]==arr[i])
+                return true;
+            if(arr[l]+arr[r]<arr[i])
+                l++;
+            else
+                r--;
+        }
+    }
+    return false;
+}
+
+int main() {
+	int cases;
+	cin>>cases;
+	while(cases--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i{};i<n;i++)
+	        cin>>arr[i];
+	    if(isPythTrip(arr,n))
+	        cout<<"Yes"<<endl;
+	    else 
+	        cout<<"No"<<endl;
+	}
+	return 0;
+}
