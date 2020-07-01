dc802badc121ef0f7e6113ec02fe62fd79be25f7
anishmo99 <ani10sh@gmail.com>
2020-07-01 22:08:45 +0530

    update

--- test.cpp
+++ test.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
 #include <string>
 #include <cstring>
-#include <vector>
 #include <cstdio>
-#include <algorithm>
-#include <utility>
+#include <cmath>
+#include <cstdlib>
+#include <unordered_map>
+#include <unordered_set>
 #include <map>
 #include <set>
+#include <utility>
+#include <algorithm>
+#include <vector>
+#include <stack>
 #include <queue>
+#include <list>
 using namespace std;
 
 typedef long long ll;
@@ -18,23 +24,7 @@ typedef long long ll;
 #define S second
 
 
-int numberOfCoins(vector<int> coins,int amount,int n)
-{
-    int dp[amount][n];
 
-    for(int i=0;i<n;i++)
-        dp[0][i]=1;
-
-    for(int i=1;i<n;i++)
-    {
-        for(int j=0;j<=amount;j++)
-        {
-            if(i==0)
-
-        }
-    }
-
-}
 
 
 int main()
@@ -48,18 +38,6 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int cases;
-    cin >> cases;
-    while(cases--){
-        int amount;
-        cin>>amount;
-        int n;
-        cin>>n;
-        vector<int> coins(n);
-        for(int i=0;i<n;i++)
-            cin>>v[i];
-        cout<<numberOfCoins(coins,amount,n);
-        cout<<endl;
-    }
+    
     return 0;
 }
