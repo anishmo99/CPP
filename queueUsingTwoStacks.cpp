1b010af30be664b0f39e51ec45113788ba477971
anishmo99 <ani10sh@gmail.com>
2020-07-02 20:02:00 +0530

    Create queueUsingTwoStacks.cpp

---
+++ queueUsingTwoStacks.cpp
@@ -0,0 +1,39 @@
+// The structure of the class is
+class StackQueue{
+private:
+    stack<int> s1;
+    stack<int> s2;
+public:
+    void push(int);
+    int pop();
+}; 
+
+// enqueue costly
+
+/* The method push to push element into the queue */
+void StackQueue ::push(int x)
+{
+    while (!s1.empty())
+    {
+        s2.push(s1.top());
+        s1.pop();
+    }
+
+    s1.push(x);
+
+    while (!s2.empty())
+    {
+        s1.push(s2.top());
+        s2.pop();
+    }
+}
+
+/*The method pop which return the element poped out of the queue*/
+int StackQueue ::pop()
+{
+    if (s1.empty())
+        return -1;
+    int x = s1.top();
+    s1.pop();
+    return x;
+}
