dd683322d5d64bec2b8e4aa6cf79540163f0507f
anishmo99 <ani10sh@gmail.com>
2020-07-09 17:12:55 +0530

    Update 3Sum.cpp

--- 3Sum.cpp
+++ 3Sum.cpp
@@ -47,3 +47,41 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
+
+        for (int i = 0; i < int(nums.size()) - 2; i++)
+        {
+            if (i == 0 || (i > 0 && nums[i] != nums[i - 1]))
+            {
+                int sum = 0 - nums[i];
+                int low = i + 1;
+                int high = nums.size() - 1;
+
+                while (low < high)
+                {
+                    if (nums[low] + nums[high] == sum)
+                    {
+                        ans.push_back(vector<int>{nums[i], nums[low], nums[high]});
+                        while (low < high && nums[low] == nums[low + 1])
+                            low++;
+                        while (low < high && nums[high] == nums[high - 1])
+                            high--;
+
+                        low++;
+                        high--;
+                    }
+                    else if (nums[low] + nums[high] > sum)
+                        high--;
+                    else
+                        low++;
+                }
+            }
+        }
+        return ans;
+    }
+};
