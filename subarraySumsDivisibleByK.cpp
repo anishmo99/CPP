6f1f703b86f736ecd436ef9b89e14585d81f88be
anishmo99 <ani10sh@gmail.com>
2020-07-10 12:22:36 +0530

    Update subarraySumsDivisibleByK.cpp

--- subarraySumsDivisibleByK.cpp
+++ subarraySumsDivisibleByK.cpp
@@ -37,3 +37,41 @@ public:
         return count;
     }
 };
+
+
+// GFG
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+int subarraysDivByK(int nums[], int n, int k)
+{
+    int hash[k];
+    memset(hash, 0, sizeof(hash));
+    hash[0]++;
+    int s = 0, counter = 0;
+    for (int i = 0; i < n; i++)
+    {
+        s = ((s + nums[i]) % k + k) % k;
+        counter += hash[s];
+        hash[s]++;
+    }
+    return counter;
+}
+
+int main()
+{
+    int cases;
+    cin >> cases;
+    while (cases--)
+    {
+        int n, k;
+        cin >> n >> k;
+        int arr[n];
+        for (int i = 0; i < n; i++)
+            cin >> arr[i];
+        cout << subarraysDivByK(arr, n, k) << endl;
+    }
+    return 0;
+}
