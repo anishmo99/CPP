e0e6e36b7215190b0582b99757739184acd9c437
anishmo99 <ani10sh@gmail.com>
2020-07-15 00:40:49 +0530

    Create jumpGame.cpp

---
+++ jumpGame.cpp
@@ -0,0 +1,15 @@
+class Solution
+{
+public:
+    bool canJump(vector<int> &nums)
+    {
+        int reachable = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (reachable < i)
+                return false;
+            reachable = max(i + nums[i], reachable);
+        }
+        return true;
+    }
+};
