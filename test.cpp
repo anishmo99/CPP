68cac8aaf23021498dcefd76e3b72cd27715c18a
Anish Mookherjee <anishmookherjee@Arijits-MacBook-Pro.local>
2020-07-22 00:31:35 +0530

    update

--- test.cpp
+++ test.cpp
@@ -31,7 +31,7 @@ int main()
 {
 #ifndef ONLINE_JUDGE
     freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
+    // freopen("output.txt","w",stdout);
 #endif
 
     ios_base::sync_with_stdio(false);
