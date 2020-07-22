123c56ab3c83d5e59d0457034b7bd1f6d0fb4a91
anishmo99 <ani10sh@gmail.com>
2020-07-22 17:43:22 +0530

    Update palindromeLinkedListCheck.cpp

--- palindromeLinkedListCheck.cpp
+++ palindromeLinkedListCheck.cpp
@@ -1,3 +1,53 @@
+// leetcode
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+  bool isPalindrome(ListNode *head)
+  {
+    ListNode *slow = head;
+    ListNode *fast = head;
+    while (fast and fast->next)
+    {
+      fast = fast->next->next;
+      slow = slow->next;
+    }
+
+    ListNode *node = nullptr;
+    ListNode *nxt;
+    while (slow)
+    {
+      nxt = slow->next;
+      slow->next = node;
+      node = slow;
+      slow = nxt;
+    }
+
+    while (node)
+    {
+      if (node->val != head->val)
+        return false;
+      // cout << node->val << ". ";
+      // cout << head->val << "/ ";
+      node = node->next;
+      head = head->next;
+    }
+    return true;
+  }
+};
+
+// gfg
+
 #include <iostream>
 #include <stack>
 #include <stdlib.h>
