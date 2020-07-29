55ff4d510d142d405764def32084aae516362c90
anishmo99 <ani10sh@gmail.com>
2020-07-29 08:57:01 +0530

    Update longestIncreasingSubsequenceUsingDP.cpp

--- longestIncreasingSubsequenceUsingDP.cpp
+++ longestIncreasingSubsequenceUsingDP.cpp
@@ -1,3 +1,27 @@
+// leetcode
+
+class Solution
+{
+public:
+    int lengthOfLIS(vector<int> &nums)
+    {
+        if (nums.empty())
+            return 0;
+        vector<int> dp(nums.size(), 1);
+        for (int i = 1; i < nums.size(); i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[j] < nums[i] and dp[i] < dp[j] + 1)
+                    dp[i]++;
+            }
+        }
+        return *max_element(dp.begin(), dp.end());
+    }
+};
+
+// gfg
+
 #include <iostream>
 #include <vector>
 #include <algorithm>
