b2246ec8b5bd11551f11090005c72789099e7cf2
anishmo99 <ani10sh@gmail.com>
2020-08-04 21:38:02 +0530

    Update wordBreak.cpp

--- wordBreak.cpp
+++ wordBreak.cpp
@@ -1,4 +1,4 @@
-// accepted
+// accepted leetcode
 
 class Solution
 {
@@ -31,7 +31,61 @@ public:
     }
 };
 
+// gfg
 
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+#include <string>
+using namespace std;
+
+bool wordBreak(string s, vector<string> &wordDict)
+{
+    unordered_set<string> dict(wordDict.begin(), wordDict.end());
+
+    vector<bool> dp(s.size() + 1, false);
+
+    dp[0] = true;
+
+    for (int i = 1; i <= s.size(); i++)
+    {
+        for (int j = i - 1; j >= 0; j--)
+        {
+            if (dp[j])
+            {
+                if (dict.find(s.substr(j, i - j)) != dict.end())
+                {
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+    }
+    return dp[s.size()];
+}
+
+int main()
+{
+    int cases;
+    cin >> cases;
+    while (cases--)
+    {
+        int n;
+        cin >> n;
+        vector<string> wordDict;
+        for (int i = 0; i < n; i++)
+        {
+            string temp;
+            cin >> temp;
+            wordDict.push_back(temp);
+        }
+        string str;
+        cin >> str;
+        cout << wordBreak(str, wordDict) << endl;
+    }
+    return 0;
+}
 
 // s -> "aaaaaaa"
 // dict -> [ "aaaa", "aaa" ]
