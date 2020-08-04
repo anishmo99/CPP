7c2386ce0fe4650cecf63f100ab1a2076684480d
anishmo99 <ani10sh@gmail.com>
2020-08-04 10:38:10 +0530

    Update maxConsecutiveOnesIII.cpp

--- maxConsecutiveOnesIII.cpp
+++ maxConsecutiveOnesIII.cpp
@@ -1,20 +1,22 @@
-class Solution {
+class Solution
+{
 public:
-    int longestOnes(vector<int>& a, int k) {
-        int i=0,j=0;
-        
-        while(i<a.size())
+    int longestOnes(vector<int> &a, int k)
+    {
+        int i = 0, j = 0;
+
+        while (i < a.size())
         {
-            if(a[i]==0)
+            if (a[i] == 0)
                 k--;
-            if(k<0)
+            if (k < 0)
             {
-                if(a[j]==0)
+                if (a[j] == 0)
                     k++;
                 j++;
             }
             i++;
         }
-        return i-j;
+        return i - j;
     }
 };
