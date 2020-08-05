2da9293c8666857b501d634f887f8f3e3ae1664e
anishmo99 <ani10sh@gmail.com>
2020-08-05 16:04:30 +0530

    Create increasingSubsequences.cpp

---
+++ increasingSubsequences.cpp
@@ -0,0 +1,30 @@
+// Input: [4, 6, 7, 7]
+// Output: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7, 7], [4, 7, 7]]
+
+class Solution {
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums) {
+        vector<int> subseq;
+        unordered_set<vector<int>> res;
+        
+        dfs(nums, subseq, res, 0);
+        
+        return vector(res.begin(),res.end());
+    }
+    
+    void dfs(vector<int>& nums, vector<int>& subseq, set<vector<int>>& result, int index)
+    {
+        if(subseq.size() >= 2)
+            result.insert(subseq);
+        
+        for(int i = index; i < nums.size(); i++)
+        {
+            if(subseq.size() == 0 or nums[i] >= subseq.back())
+            {
+                subseq.push_back(nums[i]);
+                dfs(nums, subseq, result, i+1);
+                subseq.pop_back();
+            }
+        }
+    }
+};
