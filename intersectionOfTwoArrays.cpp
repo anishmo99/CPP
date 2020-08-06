80633fdc90100c64b06310b9afbe93a6f1f29fe6
anishmo99 <ani10sh@gmail.com>
2020-08-06 19:06:56 +0530

    Update intersectionOfTwoArrays.cpp

--- intersectionOfTwoArrays.cpp
+++ intersectionOfTwoArrays.cpp
@@ -37,3 +37,35 @@ public:
         return {set2.begin(), set2.end()};
     }
 };
+
+// GFG
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+int main() {
+	int cases;
+	cin >> cases;
+	while(cases --)
+	{
+	    int n1, n2;
+	    cin >> n1 >> n2;
+	    int arr1[n1], arr2[n2];
+	    unordered_set<int> set1, set2;
+	    int count = 0;
+	    for(int i = 0; i < n1; i++)
+	    {
+	        cin >> arr1[i];
+	        set1.insert(arr1[i]);
+	    }
+	    for(int i = 0; i < n2; i++)
+	    {
+	        cin >> arr2[i];
+	        if(set1.find(arr2[i]) != set1.end())
+	            set2.insert(arr2[i]);
+	    }
+	    cout << set2.size() << endl;
+	}
+	return 0;
+}
