bc3901e6295a5f273e99697fe31311e7b1ed0ad6
anishmo99 <ani10sh@gmail.com>
2020-08-08 17:20:14 +0530

    Update findDuplicatesInArray.cpp

--- findDuplicatesInArray.cpp
+++ findDuplicatesInArray.cpp
@@ -1,3 +1,4 @@
+// O(N*logN)
 vector<int> duplicates(int nums[], int n)
 {
     vector<int> sol;
@@ -14,6 +15,35 @@ vector<int> duplicates(int nums[], int n)
         }
     }
     if (sol.empty())
+        sol.push_back(-1);
+    return sol;
+}
+
+// gfg
+
+// 2
+// 4
+// 0 3 1 2
+// 5
+// 2 3 1 2 3
+
+// O(N)
+
+vector<int> duplicates(int a[], int n) {
+    vector<int> sol;
+    for (int i = 0; i < n; i++) 
+    { 
+        a[a[i] % n] += n; 
+    } 
+    
+    for (int i = 0; i < n; i++)  
+    { 
+        if (a[i] >= n*2)  
+        { 
+            sol.push_back(i);
+        } 
+    } 
+    if(sol.empty())
         sol.push_back(-1);
     return sol;
 }
