4946f56f7ba369bb0a359496e08ec89b541acff1
Anish Mookherjee <59157112+anishmo99@users.noreply.github.com>
2020-08-12 15:08:43 +0530

    Update pathSum2.cpp

--- pathSum2.cpp
+++ pathSum2.cpp
@@ -9,6 +9,45 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
+// accepted and efficient, using pop_back() concept
+
+class Solution {
+public:
+    void dfs(TreeNode *root, int& sum, int current_sum, vector<int>& vec, vector<vector<int>>& sol){
+        if(!root)
+            return;
+        
+        current_sum += root->val;
+        vec.push_back(root->val);
+        
+        if(!root->left and !root->right and current_sum == sum ){
+            sol.emplace_back(vec);
+        }
+        
+        if(root->left)
+            dfs(root->left, sum, current_sum, vec, sol);
+        if(root->right)
+            dfs(root->right, sum, current_sum, vec, sol);
+        
+        vec.pop_back();
+    }
+    vector<vector<int>> pathSum(TreeNode* root, int sum) {
+        vector<vector<int>> sol;
+        if(root == nullptr)
+            return sol;
+        
+        vector<int> vec;
+        
+        int current_sum = 0;
+        
+        dfs(root, sum, current_sum, vec, sol);
+        
+        return sol;
+    }
+};
+
+// accepted but slower
 class Solution {
 public:
     void dfs(TreeNode *root, int& sum, int current_sum, vector<int> vec, vector<vector<int>>& sol){
