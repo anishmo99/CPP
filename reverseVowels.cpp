9733d3fc4e41ec2fc3b0e2d7c1557999050ad943
anishmo99 <ani10sh@gmail.com>
2020-09-14 15:58:53 +0530

    Update reverseVowels.cpp

--- reverseVowels.cpp
+++ reverseVowels.cpp
@@ -1,12 +1,35 @@
-#include <cmath>
-#include <cstdio>
+#include <set>
 #include <vector>
 #include <iostream>
 #include <algorithm>
 using namespace std;
 
+int main()
+{
 
-int main() {
-    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    string s;
+    cin >> s;
+    set<char> set = {'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'};
+    vector<char> v;
+
+    for (char c : s)
+    {
+        if (set.find(c) != set.end())
+        {
+            v.push_back(c);
+        }
+    }
+    // v = [u, i, i]
+    int j = v.size() - 1;
+    // [t r u m p i s s h i t]
+    for (int i = 0; i < s.size(); i++)
+    {
+        if (set.find(s[i]) != set.end())
+        {
+            s[i] = v[j];
+            j--;
+        }
+    }
+    cout << s;
     return 0;
 }
