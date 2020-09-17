c50933b1cfc23597f9cf0ca5ccfa57de00876bc6
anishmo99 <ani10sh@gmail.com>
2020-09-17 16:59:20 +0530

    Create knightProbabilityInChessboard.cpp

---
+++ knightProbabilityInChessboard.cpp
@@ -0,0 +1,32 @@
+class Solution
+{
+private:
+    int dirs[8][2] = {{2, 1}, {2, -1}, {1, 2}, {1, -2}, {-2, 1}, {-2, -1}, {-1, 2}, {-1, -2}};
+
+public:
+    double knightProbability(int N, int K, int r, int c)
+    {
+        vector<vector<double>> m(N, vector<double>(N, 1));
+        while (K--)
+        {
+            vector<vector<double>> next(N, vector<double>(N, 0));
+            for (int i = 0; i < N; i++)
+            {
+                for (int j = 0; j < N; j++)
+                {
+                    for (auto &dir : dirs)
+                    {
+                        int x = i + dir[0];
+                        int y = j + dir[1];
+                        if (x < 0 or x >= N or y < 0 or y >= N)
+                            continue;
+                        next[i][j] += m[x][y];
+                    }
+                    next[i][j] /= 8;
+                }
+            }
+            m = next;
+        }
+        return m[r][c];
+    }
+};
