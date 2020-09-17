72ca1ac1bf9ada65ba02a05485dfb7b644473d9d
anishmo99 <ani10sh@gmail.com>
2020-09-17 16:59:14 +0530

    Update

--- test1.cpp
+++ test1.cpp
@@ -1,47 +1,10 @@
-#include <iostream>
-#include <cstring>
-
-using namespace std;
-
-int strStr(string haystack, string needle) {
-    int i = 0,j = 0;
-    bool flag = false;
-    int pos = 0;
-    if(haystack.length() == 0 and needle.length() == 0)
-        return 0;
-    if(haystack.length() == 0 and needle.length() > 0)
-        return -1;
-    if(haystack.length() > 0 and needle.length() == 0)
-        return 0;
-    if(haystack == needle)
-        return 0;
-    
-    while(j < haystack.length())
-    {
-        if(i == needle.length())
-            return pos;
-        if(haystack.at(j) == needle.at(i))
-        {
-            if(!flag)
-            {
-                flag = true;
-                pos = j;
-            }
-            i++;
-            // cout << haystack.at(j);
-        }
-        else
-        {
-            i = 0;
-        }
-        j++;
+int NewNumber{int n}{
+    int res;
+    int k = 0;
+    while(n!=0){
+        int r = n%10;
+        res = (9-r)*pow(10,k++)+res;
+        n /= 10;
     }
-    return pos ? pos : -1;
-}
-
-
-int main(){
-    string haystack, needle;
-    cin >> haystack >> needle;
-    cout << strStr(haystack, needle);
+    return res;
 }
