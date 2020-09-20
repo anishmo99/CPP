621684cb98d92f1d84fffd71678b7a2f7ff24fdd
anishmo99 <ani10sh@gmail.com>
2020-09-20 20:35:40 +0530

    Update spiralMatrix.cpp

--- spiralMatrix.cpp
+++ spiralMatrix.cpp
@@ -1,43 +1,53 @@
 class Solution
 {
 public:
-    vector<int> spiralOrder(vector<vector<int>> &matrix){
-        vector<int> temp;
-        
-        if(matrix.empty())
-            return temp;
+	vector<int> spiralOrder(vector<vector<int>> &matrix)
+	{
+		vector<int> temp;
 
-        int top = 0, down = matrix.size() - 1, left = 0, right = matrix[0].size() - 1;
-        int dir = 0;
+		if (matrix.empty())
+			return temp;
 
-        while(top <= down and left <= right){
-        	if(dir == 0){
-        		for(int i = left; i <= right; i++){
-        			temp.push_back(matrix[top][i]);
-        		}
-        		top++;
-        	}
-        	else if(dir == 1){
-        		for(int i = top; i <= down; i++){
-        			temp.push_back(matrix[i][right]);
-        		}
-        		right--;
-        	}
-        	else if(dir == 2){
-        		for(int i = right; i >= left; i--){
-        			temp.push_back(matrix[down][i]);
-        		}
-        		down--;
-        	}
-        	else if(dir == 3){
-        		for(int i = down; i >= top; i--){
-        			temp.push_back(matrix[i][left]);
-        		}
-        		left++;
-        	}
+		int top = 0, down = matrix.size() - 1, left = 0, right = matrix[0].size() - 1;
+		int dir = 0;
 
-        	dir = (dir + 1) % 4;
-        }
-        return temp;
-    }
+		while (top <= down and left <= right)
+		{
+			if (dir == 0)
+			{
+				for (int i = left; i <= right; i++)
+				{
+					temp.push_back(matrix[top][i]);
+				}
+				top++;
+			}
+			else if (dir == 1)
+			{
+				for (int i = top; i <= down; i++)
+				{
+					temp.push_back(matrix[i][right]);
+				}
+				right--;
+			}
+			else if (dir == 2)
+			{
+				for (int i = right; i >= left; i--)
+				{
+					temp.push_back(matrix[down][i]);
+				}
+				down--;
+			}
+			else if (dir == 3)
+			{
+				for (int i = down; i >= top; i--)
+				{
+					temp.push_back(matrix[i][left]);
+				}
+				left++;
+			}
+
+			dir = (dir + 1) % 4;
+		}
+		return temp;
+	}
 };
