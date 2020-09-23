7e2175f2534ee6cf631b7dd80301520d8ad8014e
anishmo99 <ani10sh@gmail.com>
2020-09-23 17:58:27 +0530

    Update longestPalindromicSubstring.cpp

--- longestPalindromicSubstring.cpp
+++ longestPalindromicSubstring.cpp
@@ -3,92 +3,100 @@ class Solution
 public:
     string longestPalindrome(string s)
     {
-        if (s == "")
-            return "";
-        int n = s.length();
-        bool dp[n][n];
-        memset(dp, 0, sizeof(dp));
-        int maxlength = 1;
-        for (int i = 0; i < n; i++)
+        if (s.empty())
+            return string();
+
+        vector<vector<bool>> dp(s.length(), vector<bool>(s.length(), false));
+        // memset(dp, 0, sizeof(dp));
+
+        int start = 0, max_length = 1;
+
+        for (int i = 0; i < s.length(); i++)
+        {
             dp[i][i] = true;
-        int start = 0;
-        for (int i = 0; i < n; i++)
+        }
+
+        for (int i = 0; i < s.length(); i++)
+        {
             if (s[i] == s[i + 1])
             {
                 dp[i][i + 1] = true;
                 start = i;
-                maxlength = 2;
+                max_length = 2;
             }
-        for (int k = 3; k <= n; k++)
+        }
+
+        for (int k = 3; k <= s.length(); k++)
         {
-            for (int i = 0; i < n - k + 1; i++)
+            for (int i = 0; i <= s.length() - k; i++)
             {
                 int j = i + k - 1;
-                if (s[i] == s[j] && dp[i + 1][j - 1])
+                if (s[i] == s[j] and dp[i + 1][j - 1])
                 {
                     dp[i][j] = true;
-                    if (k > maxlength)
+                    if (k > max_length)
                     {
                         start = i;
-                        maxlength = k;
+                        max_length = k;
                     }
                 }
             }
         }
-        string res = s.substr(start, maxlength);
-        return res;
+
+        return s.substr(start, max_length);
     }
 };
 
-/*
- //geeksforgeeks
- #include <iostream>
- #include <cstring>
- using namespace std;
 
- string longestPalindrome(string s)
- {
-     if(s=="")   return "";
-     int n=s.length();
-     int flag=0;
-     bool dp[n][n];
-     memset(dp,0,sizeof(dp));
-     
-     for(int i=0;i<n;i++)
-         dp[i][i]=true;
-     
-     int start=0,maxlength=1;
-     for(int i=0;i<n-1;i++)
-         if(s[i]==s[i+1])
-         {
-             dp[i][i+1]=true;
-             if(flag==0)
-             {
-                 start=i;
-                 maxlength=2;
-                 flag++;
-             }
-         }
-     
-     for(int k=3;k<=n;k++)
-     {
-         for(int i=0;i<n-k+1;i++)
-         {
-             int j=i+k-1;
-             if(s[i]==s[j]&&dp[i+1][j-1]==true)
-             {
-                 dp[i][j]=true;
-                 if(k>maxlength)
-                 {
-                     maxlength=k;
-                     start=i;
-                 }
-             }
-         }
-     }
-     string res=s.substr(start,maxlength);
-     return res;
- }
+ //geeksforgeeks
+
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+string longestPalindrome(string s)
+{
+    if(s=="")   return "";
+    int n=s.length();
+    int flag=0;
+    bool dp[n][n];
+    memset(dp,0,sizeof(dp));
+    
+    for(int i=0;i<n;i++)
+        dp[i][i]=true;
+    
+    int start=0,maxlength=1;
+    for(int i=0;i<n-1;i++)
+        if(s[i]==s[i+1])
+        {
+            dp[i][i+1]=true;
+            if(flag==0)
+            {
+                start=i;
+                maxlength=2;
+                flag++;
+            }
+        }
+    
+    for(int k=3;k<=n;k++)
+    {
+        for(int i=0;i<n-k+1;i++)
+        {
+            int j=i+k-1;
+            if(s[i]==s[j]&&dp[i+1][j-1]==true)
+            {
+                dp[i][j]=true;
+                if(k>maxlength)
+                {
+                    maxlength=k;
+                    start=i;
+                }
+            }
+        }
+    }
+    string res=s.substr(start,maxlength);
+    return res;
+}
 
  int main() {
      int cases;
@@ -101,4 +109,3 @@ public:
      }
      return 0;
  }
-*/
