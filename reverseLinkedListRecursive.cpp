447c5c060f788d9c3746de4d5fa194ea8da42ddb
anishmo99 <ani10sh@gmail.com>
2020-09-29 00:00:01 +0530

    Create reverseLinkedListRecursive.cpp

---
+++ reverseLinkedListRecursive.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution
+{
+public:
+    ListNode *reverseList(ListNode *head)
+    {
+        if (head == nullptr or head->next == nullptr)
+        {
+            return head;
+        }
+        else
+        {
+            auto new_head = reverseList(head->next);
+            auto new_tail = head->next;
+            new_tail->next = head;
+            head->next = nullptr;
+            return new_head;
+        }
+    }
+};
