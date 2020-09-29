78bc019477386cfdb375b9e27ba6ce64e7f5bbd1
Anish Mookherjee <59157112+anishmo99@users.noreply.github.com>
2020-09-29 11:24:28 +0530

    Update rotateLinkedList.cpp

--- rotateLinkedList.cpp
+++ rotateLinkedList.cpp
@@ -1,46 +1,37 @@
-//
-//  rotateLinkedList.cpp
-//  C++
-//
-//  Created by Anish Mookherjee on 18/03/20.
-//  Copyright © 2020 Anish Mookherjee. All rights reserved.
-//
-
-
-#include <iostream>
-
-using namespace std;
-
- struct Node {
- int data;
- struct Node *next;
- Node(int x) {
- data = x;
- next = NULL;
- }
- };
-
-/*  This function should rotate list counter-clockwise
- by k and return new head (if changed) */
-
-Node* rotate(Node* head, int k)
-{
-    Node *ptr1=head,*ptr2;
-    int count=1;
-    while(count<k && ptr1!=NULL)
-    {
-        ptr1=ptr1->next;
-        count++;
-    }
-    if(ptr1==NULL)
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* rotateRight(ListNode* head, int k) {
+        if(!head or !head->next or k == 0)
+            return head;
+        
+        auto *cur = head;
+        int len = 1;
+        
+        while(cur->next){
+            cur = cur->next;
+            len++;
+        }
+        
+        k = len - k % len;
+        cur->next = head;
+        
+        while(k--){
+            cur = cur->next;
+        }
+        
+        head = cur->next;
+        cur->next = nullptr;
+        
         return head;
-    ptr2=ptr1;
-    while(ptr1->next!=NULL)
-    {
-        ptr1=ptr1->next;
     }
-    ptr1->next=head;
-    head=ptr2->next;
-    ptr2->next=NULL;
-    return head;
-}
+};
