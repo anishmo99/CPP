467263161170aed20aac2e7022fcfb3e21b0dc29
Anish Mookherjee <59157112+anishmo99@users.noreply.github.com>
2020-10-03 08:28:04 +0530

    Update and rename std_set.cpp to setSTL.cpp

--- setSTL.cpp
+++ setSTL.cpp
@@ -1,18 +1,19 @@
 #include<iostream>
 #include<set>
 #include<functional>
+
 using namespace std;
 class Person{
     public:
     int age;
     string name;
-    bool operator < (const Person &rhs) const { return age<rhs.age; }
+    // bool operator < (const Person &rhs) const { return age<rhs.age; }
     bool operator > (const Person &rhs) const { return age>rhs.age; }
 };
 int main()
 {
     // set <int , greater<>> s1 = {1,2,3,5,4,6,2,1,3,6,5,4};
-    set <Person, greater<>> p1 = {{23, "Raju"}, {17, "Parth"}, {11, "Darshan"}};
+    set <Person, greater<>> p1 = {{17, "Parth"}, {23, "Raju"}, {11, "Darshan"}};
     for(const auto & x: p1)
         cout<<x.age<<" "<<x.name<<endl;
 }
