b9ce4966410ff343b16cb58716a0212d4a657cd7
anishmo99 <ani10sh@gmail.com>
2020-10-29 13:38:58 +0530

    Update jumpGame2.cpp

--- jumpGame2.cpp
+++ jumpGame2.cpp
@@ -1,2 +1,18 @@
- 
- 
+class Solution
+{
+public:
+    int jump(vector<int> &nums)
+    {
+        int jumps = 0, curend = 0, curfurthest = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            curfurthest = max(curfurthest, i + nums[i]);
+            if (i == curend)
+            {
+                jumps++;
+                curend = curfurthest;
+            }
+        }
+        return jumps;
+    }
+};
