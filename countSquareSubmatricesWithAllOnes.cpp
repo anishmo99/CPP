61cf6e76f27c235a4b3e80a0a94bf5f98a953479
anishmo99 <ani10sh@gmail.com>
2020-10-29 13:38:44 +0530

    Update countSquareSubmatricesWithAllOnes.cpp

--- countSquareSubmatricesWithAllOnes.cpp
+++ countSquareSubmatricesWithAllOnes.cpp
@@ -48,3 +48,34 @@ public:
         return count;
     }
 };
+
+// OR
+
+class Solution
+{
+public:
+    // refer - https://www.youtube.com/watch?v=ojz8xZc8pog&ab_channel=thecodingworld
+
+    int countSquares(vector<vector<int>> &matrix)
+    {
+
+        int row = matrix.size();
+        int col = matrix[0].size();
+
+        int sum = 0;
+
+        vector<vector<int>> v(row + 1, vector<int>(col + 1, 0));
+
+        for (int i = 1; i <= row; i++)
+            for (int j = 1; j <= col; j++)
+            {
+                if (matrix[i - 1][j - 1] == 1)
+                {
+                    v[i][j] = 1 + min(v[i - 1][j], min(v[i][j - 1], v[i - 1][j - 1]));
+                    sum += v[i][j];
+                }
+            }
+
+        return sum;
+    }
+};
