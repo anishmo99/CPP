5548680ae0b464e378a2c3554f54fc08ea9a2e5c
anishmo99 <ani10sh@gmail.com>
2021-04-28 08:33:44 +0530

    Create nthTribonacciNumber.cpp

---
+++ nthTribonacciNumber.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int tribonacci(int n) {
+        
+        int a = 0, b = 1, c = 1, d;
+        
+        if(n == 0)
+            return a;
+        
+        if(n == 1)
+            return b;
+        
+        if(n == 2)
+            return c;
+        
+        for(int i = 3; i <= n; i++){
+            d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return d;
+    }
+};
