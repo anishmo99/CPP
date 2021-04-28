c3df142af98213428496e018714eea0e3e853793
anishmo99 <ani10sh@gmail.com>
2021-04-28 08:33:46 +0530

    Update findAllAnagramsInString.cpp

--- findAllAnagramsInString.cpp
+++ findAllAnagramsInString.cpp
@@ -3,6 +3,53 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
+        unordered_map<char, int> maps;
+        unordered_map<char, int> mapp;
+        
+        for(int i = 0; i < p.size(); i++)
+            mapp[p[i]]++;
+        
+        
+        vector<int> ans;
+        int kp = p.size();
+        int ks = 0;
+        int i = 0, j = 0;
+        
+        while(j < s.length()){
+            if(ks < kp){
+                ks++;
+                maps[s[j]]++;
+                j++;
+            }
+            
+            if(ks == kp and maps != mapp){
+                maps[s[i]]--;
+                if(maps[s[i]] == 0)
+                    maps.erase(s[i]);
+                ks--;
+                i++;
+            }
+            
+            else if(maps == mapp){
+                ans.push_back(i);
+                maps[s[i]]--;
+                if(maps[s[i]] == 0)
+                    maps.erase(s[i]);
+                ks--;
+                i++;
+            }
+            
+        }
+        
+        return ans;
+    }
+};
+
+// or
+
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
         vector<int> res;
         if(s.length()==0||p.length()>s.length())  return res;
         int hash[26]={0};
