256c9e909a763b7e41a47a2ac211503544a03746
anishmo99 <ani10sh@gmail.com>
2021-04-29 11:03:50 +0530

    Update minimumPathSum.cpp

--- minimumPathSum.cpp
+++ minimumPathSum.cpp
@@ -19,3 +19,28 @@ public:
         return grid[r - 1][c - 1];
     }
 };
+
+// memoization
+
+class Solution {
+public:
+    vector<vector<int>> dp;
+    int solve(vector<vector<int>>& grid, int m, int n){
+        if(m < 0 or n < 0)
+            return INT_MAX;
+        
+        if(m == 0 and n == 0)
+            return dp[m][n] = grid[m][n];
+        
+        if(dp[m][n] != -1)
+            return dp[m][n];
+        
+        return dp[m][n] = grid[m][n] + min(solve(grid, m - 1, n), solve(grid, m, n - 1));
+        
+    }
+    int minPathSum(vector<vector<int>>& grid) {
+        dp.resize(grid.size(), vector<int> (grid[0].size(), -1));
+        
+        return solve(grid, grid.size() - 1, grid[0].size() - 1);
+    }
+};
