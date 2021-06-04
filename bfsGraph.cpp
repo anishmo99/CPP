81c7be1e6230045b7d1d6794fb8ec86ed36b3436
anishmo99 <ani10sh@gmail.com>
2021-06-04 10:43:20 +0530

    Update bfsGraph.cpp

---
+++ bfsGraph.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <map>
+#include <vector>
+#include <utility>
+#include <list>
+
+using namespace std;
+
+map<int, vector<int>> adjlist;
+
+void addEdge(int x, int y)
+{
+    adjlist[x].push_back(y);
+}
+
+void displayEdge()
+{
+    for (auto x : adjlist)
+    {
+        cout << x.first << " --> ";
+        for (auto y : x.second)
+        {
+            cout << y << " ";
+        }
+        cout << endl;
+    }
+}
+
+void BFS(int x){
+    queue<int>
+}
+
+int main()
+{
+    int nodes, edges;
+    cin >> nodes >> edges;
+
+    int x, y;
+
+    for (int i = 0; i < edges; i++)
+    {
+        cin >> x >> y;
+
+        addEdge(x, y);
+    }
+
+    displayEdge();
+
+    return 0;
+}
